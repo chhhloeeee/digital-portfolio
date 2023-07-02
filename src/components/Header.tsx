c33f48956e38eb3ef512cadc6d8214c7f0431b30
Chloe Baker <chloe.baker@huler.io>
2023-07-02 18:29:23 +0100

    fix: header sizing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,21 +16,48 @@ const Header = ({ className, id }: HeaderProps) => {
 };
 
 const StyledHeader = styled(Header)`
-  min-height: 100vh;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   color: black;
-  font-size: 70px;
   background-image: url(${OrgangeGradient});
   background-repeat: no-repeat;
   background-position: center;
+  background-size: contain;
   padding-bottom: 3rem;
+  h1 {
+    font-size: 120px;
+    margin: 0;
+  }
   p {
     font-family: "Poppins", sans-serif;
     font-size: 30px;
-    margin-top: -7rem;
+    margin: 0;
+  }
+  @media (max-width: 1000px) {
+    background-size: cover;
+    h1 {
+      font-size: 90px;
+    }
+  }
+  @media (max-width: 750px) {
+    h1 {
+      font-size: 80px;
+    }
+    p {
+      font-size: 25px;
+    }
+  }
+  @media (max-width: 450px) {
+    padding-top: 1rem;
+    h1 {
+      font-size: 60px;
+    }
+    p {
+      font-size: 20px;
+    }
   }
 `;
 
